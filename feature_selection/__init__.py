4059a021d3ef09130f1efb14fa56079884464bca
Willian Watanabe <watinha@gmail.com>
2020-08-25 17:29:38 -0300

    further refactoring

--- feature_selection/__init__.py
+++ feature_selection/__init__.py
@@ -3,11 +3,11 @@ import random
 from sklearn.feature_extraction.text import CountVectorizer,TfidfVectorizer
 from sklearn.tree import DecisionTreeClassifier
 
-class USES:
+class FeatureSearch:
 
   def __init__(self, n=50, language='english', max_iteration=50,
                classifier=DecisionTreeClassifier(), extractor=TfidfVectorizer(),
-               random_state=42):
+               random_state=42, strategy=None):
     self._language = language
     self._score = []
     self._n = n
@@ -17,6 +17,7 @@ class USES:
     self._best_acc = 0
     self._best_features = []
     self._random_state = random_state
+    self._strategy = strategy
 
 
   def _word_count (self, X, y):
@@ -50,33 +51,15 @@ class USES:
 
     return (severity0, severity1, all_words, map0, map1)
 
-  def _calculate_aff_score(self, severity0, severity1, all_words, map0, map1):
-    for word in all_words:
-      n_0 = len(severity0)
-      n_1 = len(severity1)
-      n_w = map0[word] + map1[word]
-      n_0_w = map0[word]
-      n_1_w = map1[word]
-
-      aff_0_w = n_0_w / (n_0 + n_w - n_0_w)
-      aff_1_w = n_1_w / (n_1 + n_w - n_1_w)
-
-      self._score.append({ 'word': word, 'score': (aff_0_w - aff_1_w) })
-
-    self._score = sorted(self._score, key=lambda x: x['score'])
-
-  def _sample(self, features_subset, iteration):
-    return random.sample(features_subset, self._n)
-
   def fit(self, X, y):
     (severity0, severity1, all_words, map0, map1) = self._word_count(X, y)
-    self._calculate_aff_score(severity0, severity1, all_words, map0, map1)
+    self._score = self._strategy._calculate_aff_score(severity0, severity1, all_words, map0, map1)
 
     features_subset = list(map(lambda x: x['word'], self._score[:self._n] + self._score[-self._n:]))
 
     random.seed(self._random_state)
     for i in range(self._max_iteration):
-      features = self._sample(features_subset, i)
+      features = self._strategy._sample(features_subset, self._n, i)
       self._extractor.vocabulary = features
       X_new = self._extractor.fit_transform(X)
       self._classifier.fit(X_new, y)
@@ -92,3 +75,29 @@ class USES:
   def fit_transform(self, X, y):
     self.fit(X, y)
     return self.transform(X)
+
+
+class USES:
+
+  def _calculate_aff_score(self, severity0, severity1, all_words, map0, map1):
+    score = []
+
+    for word in all_words:
+      n_0 = len(severity0)
+      n_1 = len(severity1)
+      n_w = map0[word] + map1[word]
+      n_0_w = map0[word]
+      n_1_w = map1[word]
+
+      aff_0_w = n_0_w / (n_0 + n_w - n_0_w)
+      aff_1_w = n_1_w / (n_1 + n_w - n_1_w)
+
+      score.append({ 'word': word, 'score': (aff_0_w - aff_1_w) })
+
+    return sorted(score, key=lambda x: x['score'])
+
+
+  def _sample(self, features_subset, n, iteration):
+    return random.sample(features_subset, n)
+
+
